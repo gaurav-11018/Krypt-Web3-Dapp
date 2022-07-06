6c16b155f8e4fd0022016d1badccdeda23ec2ec3
gaurav-11018 <79459872+gaurav-11018@users.noreply.github.com>
2022-07-06 23:52:39 +0530

    Add files via upload

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,8 +16,6 @@ const App = () => {
         <Welcome />
       </div>
       <Services />
-      <Transactions />
-      <Footer />
     </div>
   );
 };
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
-export const contractAddress = "0xfCCF80344a668b72ac4Be23513F0E9E4a35C84fA";
+export const contractAddress = "0xad701116D4FC0c912dB9eA17d7E6355E6130E74a";
 export const contractABI = abi.abi;
